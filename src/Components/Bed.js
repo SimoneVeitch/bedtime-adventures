661abd0745b3335e4b545340150a5244a5dbae12
Simone Veitch <simone.veitch@gmail.com>
2024-07-04 19:59:13 +1000

    Added gifs and conditional logic regarding which to display

--- src/Components/Bed.js
+++ src/Components/Bed.js
@@ -1,14 +1,45 @@
 import React from "react";
+import gifStart from "../gifs/girl_idle_start.gif";
+import gifSmile from "../gifs/girl_idle_smile.gif";
+import gifAwake from "../gifs/girl_idle_awake.gif";
+import gifTired from "../gifs/girl_idle_tired.gif";
+import gifAsleep from "../gifs/girl_asleep.gif";
 
 function Bed({ points }) {
-const bedClassName = points === 100 ? "bed-sleep" : "bed";
+  let gifSrc;
+  
+  if (points === 100) {
+    gifSrc = gifAsleep;
+  } else if (points === 90) {
+    gifSrc = gifTired;
+  } else if (points === 80) {
+    gifSrc = gifTired;
+  } else if (points === 70) {
+    gifSrc = gifStart;
+  } else if (points === 60) {
+    gifSrc = gifStart;
+  } else if (points === 50) {
+    gifSrc = gifStart;
+  } else if (points === 40) {
+    gifSrc = gifStart;
+  } else if (points === 30) {
+    gifSrc = gifSmile;
+  } else if (points === 20) {
+    gifSrc = gifSmile;
+  } else if (points === 10) {
+    gifSrc = gifAwake;
+  } else {
+    gifSrc = gifStart;
+  }
 
   return (
     <div className="bed-container">
-        <div className={bedClassName}>
+      <div className="bed">
+        <img src={gifSrc} alt="Animated GIF" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+      </div>
     </div>
-  </div>
-);
+  );
 }
+
 
 export default Bed;
